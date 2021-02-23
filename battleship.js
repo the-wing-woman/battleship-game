7d432195dce1a7e785c17a7995a908e221fc1816
the-wing-woman <mercedezgoodie@gmail.com>
2021-02-23 20:39:55 +0000

    My Second Commit

--- battleship.js
+++ battleship.js
@@ -8,4 +8,18 @@ var isSunk=false;
 
 while (isSunk==false){
 	guess=prompt ("Ready, aim, fire! (Enter a number 0-6):");
+	if (guess < 0 || guess > 6) {
+		alert("Please enter a valid cell number!");
+	} else {
+guesses = guesses + 1;
+if (guess==location1 || guess ==location2 || guess ==location3){
+	hits=hits+1;
+if(hits ==3){
+	isSunk = true;
+	alert("You sank my battleship!")
+			}
+		}
+	}	
 }
+var stats = "You took " +guesses + " guesses to sink the battleship, " + " which means your shooting accuracy was " + (3/guesses);
+alert(stats);
