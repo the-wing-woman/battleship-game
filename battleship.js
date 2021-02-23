4c59ffbac1456d1b51e20f8288eddbe35d1813d8
the-wing-woman <mercedezgoodie@gmail.com>
2021-02-23 14:01:46 +0000

    My First Commit

---
+++ battleship.js
@@ -0,0 +1,11 @@
+var location1=3;
+var location2=4;
+var location3=5;
+var guess;
+var hits=0;
+var guesses=0;
+var isSunk=false;
+
+while (isSunk==false){
+	guess=prompt ("Ready, aim, fire! (Enter a number 0-6):");
+}
